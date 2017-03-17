777a43afc36234050fc1cc3cca5f2c616b8edf4e
duyhuan <huanhuan@mac.local>
2017-03-17 23:04:54 +0700

    Initial Commit

---
+++ UVENT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UVENT
+//
+//  Created by huan huan on 3/17/17.
+//  Copyright © 2017 huan huan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
